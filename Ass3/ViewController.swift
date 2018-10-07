007da3447ea25b599157b4302aa399ec344dd864
Rhythm Mecwan <rhythm813mecwan@gmail.com>
2018-10-07 12:03:51 -0700

    button auto constraints working properly

---
+++ Ass3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ass3
+//
+//  Created by Rhythm Mecwan on 10/7/18.
+//  Copyright © 2018 Rhythm Mecwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
